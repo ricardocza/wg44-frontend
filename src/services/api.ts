d2951bcf0ba8452702a82e21bc5f716426739eb4
ricardocza <ricardovcza@gmail.com>
2023-05-15 16:17:49 -0300

    feat: change baseURL for deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL: "https://wg44-ricardocza.onrender.com/api/market/new/database/",
-    baseURL: "http://127.0.0.1:8000/api/market/new/database/",
+    baseURL: "https://wg44-ricardocza.onrender.com/api/market/new/database/",
+    // baseURL: "http://127.0.0.1:8000/api/market/new/database/",
     timeout: 7000
     }
 )
